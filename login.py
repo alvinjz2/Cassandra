0d82d6491635b58278ffc03650e6b63288970df1
Alvin Zhang <alvin.zhang295@gmail.com>
2022-06-04 13:21:02 -0700

    Login works
    Error seemed to be with .env giving the wrong value for username. Note: don't name the login, "username"

--- login.py
+++ login.py
@@ -1,26 +1,13 @@
 import os
-import asyncio
+import steam.webauth as wa
 from dotenv import load_dotenv
 
 load_dotenv()
-steamguard = {
-    "steamid": "wxlydf",
-    "shared_secret": "UmHpWrsHMOUgWfjncJBobD8iqds=",
-    "identity_secret": "i48bNdh8nEI8J2j99SzQBBSsAo4=",
-}
-from asyncsteampy.client import SteamClient as AsyncSteamClient
-username, password, api_key = os.environ.get('username'), os.environ.get('password'), os.environ.get('steam_api_key')
+user = wa.WebAuth(os.environ.get("id"))
 
-
-async def signin(username, password, steamguard, api_key):
-    async_steam_client = AsyncSteamClient(username, password, steamguard, api_key=api_key)
-    try:
-        await async_steam_client.login()
-        print('Success')
-        await async_steam_client.close()
-    except:
-        print('Unsuccessful')
-        await async_steam_client.close()
-
-
-asyncio.run(signin(username, password, steamguard, api_key))
+try:
+    session = user.cli_login(os.environ.get("password"))
+    session.get('https://store.steampowered.com/account/history')
+    print('Success')
+except:
+    print('Unable to get session')
