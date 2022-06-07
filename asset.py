56b0f07d66471c9b2a16cba55b572d40285dfc59
Alvin Zhang <alvin.zhang295@gmail.com>
2022-06-06 19:01:00 -0700

    Update asset.py
    Add steam64id to the tuple to be returned

--- asset.py
+++ asset.py
@@ -1,5 +1,5 @@
 from scraper import browse
-
+from utils import partner_to_64id
 class Asset:
     def __init__(self, link):
         self.link = link
@@ -14,6 +14,7 @@ class Asset:
         bid, ask = abs(self.bids[0][0][0]), abs(self.asks[0][0][0])
         bid_offer, ask_offer =  self.bids[0][1], self.asks[0][1]
         bid_t, ask_t = self.bids[0][0][1], self.asks[0][0][1]
+        bid_id, ask_id = partner_to_64id(bid_offer), partner_to_64id(ask_offer)
         if bid > ask:
-            return (True, bid, ask, bid_t, ask_t, bid_offer, ask_offer)
-        return (False, bid, ask, bid_t, ask_t, bid_offer, ask_offer)
+            return (True, bid, ask, bid_t, ask_t, bid_offer, ask_offer, bid_id, ask_id)
+        return (False, bid, ask, bid_t, ask_t, bid_offer, ask_offer, bid_id, ask_id)
