ae1428de26aa1ea7fc5dedce9357bb1899170feb
Alvin Zhang <alvin.zhang295@gmail.com>
2022-06-07 08:30:36 -0700

    Utils
    Add option enum to use for better readability purposes

---
+++ AssetClass.py
@@ -0,0 +1,20 @@
+from Scraper import browse
+from utils import partner_to_64id
+
+class AssetClass:
+    def __init__(self, link):
+        self.link = link
+        self.bids, self.asks = None, None
+        
+    def update(self):
+        res = browse(self.link)
+        self.bids, self.asks = res[1], res[0]
+
+    def opportunity(self):
+        self.update()
+        bid, ask = abs(self.bids[0][0][0]), abs(self.asks[0][0][0])
+        tup = (False, bid, ask, self.bids[0][0][1], self.asks[0][0][1],
+                     self.bids[0][1], self.asks[0][0][1], partner_to_64id(self.bids[0][1]), partner_to_64id(self.asks[0][1]))
+        if bid > ask:
+           tup[0] = True
+        return tup
--- utils.py
+++ utils.py
@@ -1,4 +1,19 @@
+from enum import Enum
+
 def partner_to_64id(trade_url):
     x = trade_url.split('?')[1].split('&')[0].split('=')[1]
     return (1 << 56) | (1 << 52) | (1 << 32) | int(x)
     
+class Option(Enum):
+    opportunity = 0
+    bid = 1
+    ask = 2
+    bid_currency = 3
+    ask_currency = 4
+    bid_url = 5
+    ask_url = 6
+    bid_64id = 7
+    ask64_id = 8
+   
+# Look to implement database catalog with name/ids in the future
+item_class_ids = {'scrap': 2675, 'key': 101785959, 'ref': 2674, 'rec': 5564, '2675': 'scrap', '101785959' : 'key', '2674' : 'ref', '5564' : 'rec'}
