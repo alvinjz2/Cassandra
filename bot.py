23e6b0fef5571ac2c52779ca314ef6e901f24fff
Alvin Zhang <alvin.zhang295@gmail.com>
2022-06-07 08:32:52 -0700

    Get inventory
    Initialize inventory on init, also add class functions to get inventory of self and other users

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 from steampy.client import SteamClient
+from utils import Option, item_class_ids
 
 class Bot:
-    def __init__(self, username, password, api_key, steamguard):
+    def __init__(self, username, password, api_key, steamguard, game):
         self.client = SteamClient(api_key)
         try:
             self.client.login(username, password, steamguard)
@@ -9,6 +10,33 @@ class Bot:
         except:
             print('Unsuccesful')
             raise SystemError
-            
-    def __del__(self):
-        self.client.logout()
+
+        self.game = game
+        self.capital, self.collateral = {'ref': 0, 'rec': 0, 'scrap': 0, 'key' : 0}, {}
+        inventory = self.get_items()
+        tag = 'classid'
+        for item in inventory.keys():
+            if inventory[item][tag] == '2674':
+                self.capital['ref'] += 1
+            if inventory[item][tag] == '5564':
+                self.capital['rec'] += 1
+            if inventory[item][tag] == '2675':
+                self.capital['scrap'] += 1
+            if inventory[item][tag] == '101785959':
+                self.capital['key'] += 1
+        
+    def get_capital(self):
+        return self.capital
+
+    def get_items(self):
+        return self.client.get_my_inventory(self.game)
+
+    def get_partner_items(self, partner_id):
+        return self.client.get_partner_inventory(partner_id, self.game)
+
+    def arbitrage(self, asset):
+        if asset[Option.opportunity]:
+            return True
+        return False
+
+
