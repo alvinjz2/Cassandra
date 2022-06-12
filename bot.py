6a3a180f789c9c791276b338526f4faa24fd637d
Alvin Zhang <alvin.zhang295@gmail.com>
2022-06-11 17:04:38 -0700

    Add function to trade and get key
    Add function that looks for a key. Scans inventory for matching ids, up to a certain quantity. Function to trade uses a price to get offered items, calls get key (for now) and sends offer

--- bot.py
+++ bot.py
@@ -1,19 +1,20 @@
+from ctypes import resize
 from steampy.client import SteamClient, Asset
 from utils import item_class_ids, pure_assets, resize_offer, pure_balance, partner_to_64id
-import math
+
 
 class Bot:
     def __init__(self, username, password, api_key, steamguard, game):
         self.client = SteamClient(api_key)
         try:
             self.client.login(username, password, steamguard)
-            print('Success')
-        except:
-            print('Unsuccesful')
-            raise SystemError
+            print('Logged in')
+        except Exception as e:
+            print(f'Could not login.\n Error message: {e}')
 
         self.game = game
-        self.capital = self.get_capital(self.client.steam_guard['steamid'])
+        self.steam_id = self.client.steam_guard['steamid']
+        self.capital = self.get_capital(self.steam_id)
         self.balance = pure_balance(len(self.capital['metals']['ref']), len(self.capital['metals']['rec']), len(self.capital['metals']['scrap']))
 
     def get_capital(self, steam64id):
@@ -31,14 +32,33 @@ class Bot:
                 capital['other']['key'].append(Asset(item, self.game))
         return capital
 
+    def find_key(self, tradeurl, quantity):
+        steam64id = partner_to_64id(tradeurl)
+        inventory = self.client.get_partner_inventory(steam64id, self.game)
+        capital, count = [], 0
+        tag = 'classid'
+        for item in inventory.keys():
+            if count < quantity:
+                if int(inventory[item][tag]) == item_class_ids['key']:
+                    capital.append(Asset(item, self.game))
+                    count += 1
+        return capital
+
+    def trade_for(self, price, item, partner_url):
+        partner = None
+        if item == 'key':
+            partner = self.find_key(partner_url, 1)
+            own = resize_offer(self.capital, price)
+            try:
+                offer = self.client.make_offer_with_url(items_from_me=own, items_from_them=partner, trade_offer_url=partner_url, 
+                                                        case_sensitive=False, message='Test sending trade offers')
+            except Exception as e:
+                print(f'Could not send offer. \n Error message: {e}')
+
+
     def arbitrage(self, asset):
-        # asset_class = asset.opportunity()
-        # if asset_class[0]:
-        #     if self.balance > asset_class[2]:
-        #         print(resize_offer(self.capital, self.balance))
-        #     print('Arbitrage possible, but not enough balance to transact')
-        ret = resize_offer(self.capital, self.balance)
-        print(ret)
-        return False
+        return None
+       
+
 
 
