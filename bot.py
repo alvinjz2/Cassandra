7c6c034e4be6e091136cd1afd6a8aca13fe0dcfc
Alvin Zhang <alvin.zhang295@gmail.com>
2022-06-16 14:39:42 -0700

    Add offer validation logic
    Check if offers have correct number of items, otherwise do not execute trade offer

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from steampy.client import SteamClient
 from utils import pure_balance, partner_to_64id, find_tf2item, get_tf2capital, resize_offer
+import time
 
 class Bot:
     def __init__(self, username, password, api_key, steamguard, game):
@@ -7,8 +8,8 @@ class Bot:
         try:
             self.client.login(username, password, steamguard)
             print('Logged in')
-        except Exception as e:
-            print(f'Could not login.\n Error message: {e}')
+        except:
+            print(f'Could not login')
         self.steam_id = self.client.steam_guard['steamid']
         self.game = game
         self.capital = get_tf2capital(self.steam_id)
@@ -18,24 +19,33 @@ class Bot:
     def trade_buy(self, quantity, item, price, partner_url):
         partner = find_tf2item(partner_to_64id(partner_url), quantity, item)
         own = resize_offer(self.capital, price)
+        if not partner or not own:
+            print('Insuffient funds')
+            return
         try:
             offer = self.client.make_offer_with_url(items_from_me=own, items_from_them=partner, trade_offer_url=partner_url, 
                                                     case_sensitive=True, message='Test sending trade offers')
             print(offer)
+            return offer
         except:
-            return
+            return None
 
 
     def trade_sell(self, quantity, item, price, partner_url):
         own = find_tf2item(self.steam_id, quantity, item)
         partner = resize_offer(get_tf2capital(partner_to_64id(partner_url)), price)
+        if not partner or not own:
+            print('Insuffient funds')
+            return
         try:
             offer = self.client.make_offer_with_url(items_from_me=own, items_from_them=partner, trade_offer_url=partner_url, 
                                                     case_sensitive=True, message='Test sending trade offers')
             print(offer)
+            return offer
         except:
-            return
+            return None
 
+    
     def arbitrage(self, asset):
         return None
        
